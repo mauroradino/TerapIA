a771542b24e9845ce2ba4248838768a3caffc5bf
mauroradino <mauroradino2003@gmail.com>
2026-01-18 15:47:54 -0300

    fix: opik

--- core/agents_openai/QA_agent.py
+++ core/agents_openai/QA_agent.py
@@ -3,11 +3,10 @@ from dotenv import load_dotenv
 from agents_openai.prompts import QA_prompt
 from agents_openai.tools import set_reminder, update_user_info
 from opik.integrations.openai import track_openai
-from openai import OpenAI
+import openai 
 load_dotenv()
 
-client = OpenAI()
-track_openai(client)
+track_openai(openai.OpenAI)
 
 
 
